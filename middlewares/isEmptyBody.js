8e863df6590658cfb07d78983d0714376dafb923
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-02-19 21:34:22 +0100

    conected with DB

---
+++ middlewares/isEmptyBody.js
@@ -0,0 +1,9 @@
+import { HttpError } from "../helpers/index.js";
+
+const isEmptyBody = (req, res, next) => {
+  if (!Object.keys(req.body).length) {
+    return next(HttpError(400, "Body is empty"));
+  }
+  next();
+};
+export default isEmptyBody;
