e1ba7ddb3f9d8e2bf5c6823fe9015f2afab4acfb
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-02-20 12:03:11 +0100

    added route api/auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRouter from "./routes/api/auth-router.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
---
+++ controllers/auth-controllers.js
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config.js";
+
+import User from "../models/User.js";
+
+import { ctrlWrapper } from "../decorators/index.js";
+import { HttpError } from "../helpers/index.js";
+
+const { JWT_SECRET } = process.env;
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const result = await User.findOne({ email });
+
+  if (result) {
+    throw HttpError(409, "Email already in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.json({
+    username: newUser.username,
+    email: newUser.email,
+  });
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrone");
+  }
+
+  const passwordCompare = bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrone");
+  }
+
+  const { _id: id } = user;
+
+  const payload = { id };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
+  res.json({ token });
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin),
+};
